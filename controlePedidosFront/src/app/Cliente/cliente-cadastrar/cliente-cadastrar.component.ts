21e899af7c2dc5fcfd56b3a6018afea0876d7704
Evelin <evelin.kashimir@gmail.com>
2021-11-05 11:59:27 -0300

    estilizando pagina inicial

--- controlePedidosFront/src/app/Cliente/cliente-cadastrar/cliente-cadastrar.component.ts
+++ controlePedidosFront/src/app/Cliente/cliente-cadastrar/cliente-cadastrar.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+
+
 @Component({
   selector: 'app-cliente-cadastrar',
   templateUrl: './cliente-cadastrar.component.html',
--- controlePedidosFront/src/app/app.module.ts
+++ controlePedidosFront/src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {Component} from '@angular/core';
+import {MatExpansionModule} from '@angular/material/expansion';
+
 import { ClienteCadastrarComponent } from './Cliente/cliente-cadastrar/cliente-cadastrar.component';
 import { ClienteListarComponent } from './Empresa/cliente-listar/cliente-listar.component';
 import { PedidosListarComponent } from './Empresa/pedidos-listar/pedidos-listar.component';
@@ -24,8 +26,11 @@ import { FazerPedidoComponent } from './Cliente/fazer-pedido/fazer-pedido.compon
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule, 
+    MatExpansionModule
+
   ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
