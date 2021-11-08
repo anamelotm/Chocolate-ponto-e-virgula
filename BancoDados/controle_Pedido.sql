221546095519e382f0b6b0dfb9c724007aa0e200
jacquelinepslima <jacquelinepslima@gmail.com>
2021-11-08 10:33:46 -0300

    atualização banco de dados e arquivos .xml com conexao com o banco

--- BancoDados/controle_Pedido.sql
+++ BancoDados/controle_Pedido.sql
@@ -47,7 +47,37 @@ select * from pedido;
 select * from item_pedido;
 select * from cliente;
 
+-- alteração das tabelas e inserção das FK's
+
 ALTER TABLE Item_Pedido add constraint fk_itemP_pedido FOREIGN KEY (codigo_pedido) REFERENCES pedido(codigo);
 ALTER TABLE Item_Pedido add constraint fk_itemP_produto FOREIGN KEY (codigo_produto) REFERENCES produto(codigo);
 ALTER TABLE Pedido add constraint fk_pedido_cliente FOREIGN KEY (codigo_cliente) REFERENCES cliente(codigo);
+
+-- inserção de produtos
+
+INSERT INTO produto (descricao_Produto, unidade_Medida , valor_Unitario , status) VALUES('Trufa de doce de leite', 'gr', 3.50, true);
+INSERT INTO produto (descricao_Produto, unidade_Medida , valor_Unitario , status) VALUES('Barra de chocolate ao leite', 'gr', 22.90, true), 
+('Pepitas com Açucar Mascavo', 'gr', 47.90, true);
+
+-- alteracao da tabela produto - insercao de url_foto e peso
+
+alter table produto add column url_Foto varchar (255) not null;
+alter table produto add column Peso double (3,2);
+alter table produto add column data_Fabricação date not null;
+alter table produto add column data_Validade date not null;
+
+-- correcao
+
+ALTER TABLE produto CHANGE data_Fabricação data_Fabricacao date not null;
+
+-- formatação das datas
+
+SELECT DATE_FORMAT(data_Fabricacao, "%d %M %Y") FROM produto;
+SELECT DATE_FORMAT(data_Validade, "%d %M %Y") FROM produto;
+
+-- atualização dos produtos
+
+UPDATE produto SET url_Foto = 'https://e7.pngegg.com/pngimages/372/321/png-clipart-chocolate-truffle-gaufrette-chocolate-truffle-chocolate.png', data_Fabricacao = '2021-11-08', data_Validade = '2024-11-08' WHERE Codigo = 1;
+
+
 
