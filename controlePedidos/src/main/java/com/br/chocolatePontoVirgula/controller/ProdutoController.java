19323bc545e2e01341744b482df531eec15e1446
jacquelinepslima <jacquelinepslima@gmail.com>
2021-11-10 09:32:47 -0300

    Atualização do projeto sem persistence.xml

--- controlePedidos/src/main/java/com/br/chocolatePontoVirgula/controller/ProdutoController.java
+++ controlePedidos/src/main/java/com/br/chocolatePontoVirgula/controller/ProdutoController.java
@@ -1,21 +1,27 @@
 package com.br.chocolatePontoVirgula.controller;
 
+import com.br.chocolatePontoVirgula.model.entity.Produto;
+import com.br.chocolatePontoVirgula.model.repository.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/produtos")
+@RequestMapping("/produtos")
 public class ProdutoController {
 
     @Autowired
-    private ProdutoController produtoController;
+    private ProdutoRepository produtoRepository;
 
-   /* @GetMapping(value = "/lista-produtos")
+   @GetMapping("/lista-produtos")
     public ResponseEntity<Page<Produto>> findAll(PageRequest pageRequest) {
         PageRequest.ofSize(10);
 
-        Page<Produto> result = ProdutoRepository.findAll(pageRequest);
+        Page<Produto> result = produtoRepository.findAll(pageRequest);
         return ResponseEntity.ok(result);
-    } */
+    }
 }
