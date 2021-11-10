5aad2827b621eaed19f79a84b6cf15612c28a1a4
evelin-kashimir <evelin.kashimir@gmail.com>
2021-11-10 14:25:21 -0300

    teste

--- controlePedidosFront/src/app/Cliente/fazer-pedido/pedido.ts
+++
@@ -1,8 +0,0 @@
-
-export class FazerPedidoComponent {
-    public cep!: number;      // propriedade que liga o html ao ts e vice-versa
-  
-    cadastrar() {
-      console.log(this.cep)  // valor inserido no input
-    }
-}
