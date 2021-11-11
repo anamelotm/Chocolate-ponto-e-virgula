e71327cfeaa95d8cbba404b63beab1fc1e58ee94
IsabelOliv <vargas.oliveira.94@gmail.com>
2021-11-11 08:39:43 -0300

    alteração controller pedido

--- controlePedidos/src/main/java/com/br/chocolatePontoVirgula/controller/PedidoController.java
+++ controlePedidos/src/main/java/com/br/chocolatePontoVirgula/controller/PedidoController.java
@@ -1,7 +1,5 @@
 package com.br.chocolatePontoVirgula.controller;
 
-import com.br.chocolatePontoVirgula.model.entity.Cliente;
-import com.br.chocolatePontoVirgula.model.entity.Produto;
 import com.br.chocolatePontoVirgula.model.repository.PedidoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -21,16 +19,7 @@ public class PedidoController {
 
     @Autowired
     private PedidoRepository pedidoRepository;
-/*
-* Codigo  			int 		not null auto_increment primary key,
-	data_Pedido -		DateTime 	not null,
-	Codigo_cliente - 		int 		not null,
-	endereco_Entrega - 	varchar (150),
-	Situacao 		-	boolean 	not null,
-	valor_Total - 		double (8,2) not null,
-	quantidade_Total -	int 		not null,
-	percentual_Desconto - int
-* */
+
     @PostMapping
     public @ResponseBody
     String addNewUser(@RequestParam Date dataPedido
