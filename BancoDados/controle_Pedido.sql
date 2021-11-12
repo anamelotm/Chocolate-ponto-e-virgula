c2b0980068ebbb021119df870f82c28efcb69e3b
jacquelinepslima <jacquelinepslima@gmail.com>
2021-11-12 13:35:03 -0300

    Atualização do Banco de Dados

--- BancoDados/controle_Pedido.sql
+++ BancoDados/controle_Pedido.sql
@@ -1,50 +1,53 @@
+drop schema controlepedidos;
 create schema controlepedidos;
 use controlepedidos;
 
 create table cliente (
-    codigo                 int         not null auto_increment primary key,
-    tipo                varchar(8) not null,
-    documento            varchar(18) not null,
-    nome                varchar(100) not null
+	codigo 				int 		not null auto_increment primary key,
+	tipo				varchar(8) not null,
+	documento			varchar(18) not null,
+	nome				varchar(100) not null
 );
 
 create table produto (
-    codigo                 int         not null auto_increment primary key,
-    nome                 varchar(100) not null,
-    descricao             text,
-    unidadeMedida         varchar (2) not null,
-    valorUnitario         double (4,2) not null,
-    status                 boolean     not null,
-    urlFotos            varchar(255) not null,
-    peso                double(5,2) not null,
-    dataFabricacao        date not null,
-    dataValidade        date not null,
-    quantidadeEstoque    int not null
+	codigo 				int 		not null auto_increment primary key,
+	nome 				varchar(100) not null,
+	descricao 			text,
+	unidade_medida 		varchar (2) not null,
+	valor_unitario 		double (4,2) not null,
+	status 				boolean 	not null,
+	url_fotos			varchar(255) not null,
+	peso				double(5,2) not null,
+	data_fabricacao		date not null,
+	data_validade		date not null,
+	quantidade_estoque	int not null
 );
+
 create table pedido(
-    codigo                 int not null auto_increment primary key,
-    codigoCliente         int not null, -- FK da coluna codigo tabela cliente
-    enderecoEntrega     varchar(150) not null,
-    dataPedido            date not null,
-    aberto                 boolean not null,
-    valorTotal            double(8,2) not null,
-    quantidadeTotal    int not null,
-    percentualDesconto    int
-    
+	codigo 				int not null auto_increment primary key,
+	codigo_cliente 		int not null, -- FK da coluna codigo tabela cliente
+	endereco_entrega 	varchar(150) not null,
+	data_pedido			date not null,
+	aberto 				boolean not null,
+	valor_total			double(8,2) not null,
+	quantidade_total	int not null,
+	percentual_desconto	int
+	
 );
+
 create table itemPedido(
-    codigo                 int not null auto_increment primary key,
-    codigoPedido        int not null,  -- FK da coluna codigo tabela pedido
-    codigoProduto        int not null,  -- FK da coluna codigo tabela produto
-    quantidade            int not null,
-    valorTotal            double(5,2) not null
+	codigo 				int not null auto_increment primary key,
+	codigo_pedido		int not null,  -- FK da coluna codigo tabela pedido
+	codigo_produto		int not null,  -- FK da coluna codigo tabela produto
+	quantidade			int not null,
+	valorTotal			double(5,2) not null
 );
 
 -- alteração das tabelas e inserção das FK's
 
-ALTER TABLE itemPedido add constraint fk_itemP_pedido FOREIGN KEY (codigoPedido) REFERENCES pedido(codigo) on delete cascade;
-ALTER TABLE itemPedido add constraint fk_itemP_produto FOREIGN KEY (codigoProduto) REFERENCES produto(codigo) on delete cascade;
-ALTER TABLE pedido add constraint fk_pedido_cliente FOREIGN KEY (codigoCliente) REFERENCES cliente(codigo) on delete cascade;
+ALTER TABLE itemPedido add constraint fk_itemP_pedido FOREIGN KEY (codigo_pedido) REFERENCES pedido(codigo) on delete cascade;
+ALTER TABLE itemPedido add constraint fk_itemP_produto FOREIGN KEY (codigo_produto) REFERENCES produto(codigo) on delete cascade;
+ALTER TABLE pedido add constraint fk_pedido_cliente FOREIGN KEY (codigo_cliente) REFERENCES cliente(codigo) on delete cascade;
 
 desc cliente;
 desc itemPedido;
@@ -53,11 +56,11 @@ desc produto;
 
 -- inserção de produtos
 
-INSERT INTO produto (nome, unidadeMedida , valorUnitario , status, urlFotos, peso, dataFabricacao, dataValidade, quantidadeEstoque) 
+INSERT INTO produto (nome, unidade_medida , valor_unitario , status, url_fotos, peso, data_fabricacao, data_validade, quantidade_estoque) 
 VALUES('Trufa de doce de leite', 'gr', 3.50, true, 'https://e7.pngegg.com/pngimages/372/321/png-clipart-chocolate-truffle-gaufrette-chocolate-truffle-chocolate.png', 11, '2021-11-08', '2024-11-08', 12);
-INSERT INTO produto (nome, unidadeMedida , valorUnitario , status, urlFotos, peso, dataFabricacao, dataValidade, quantidadeEstoque) 
+INSERT INTO produto (nome, unidade_medida , valor_unitario , status, url_fotos, peso, data_fabricacao, data_validade, quantidade_estoque) 
 VALUES('Barra de chocolate ao leite', 'gr', 22.90, true, 'https://e7.pngegg.com/pngimages/372/321/png-clipart-chocolate-truffle-gaufrette-chocolate-truffle-chocolate.png', 250, '2021-11-08', '2024-11-08', 20); 
-INSERT INTO produto (nome, unidadeMedida , valorUnitario , status, urlFotos, peso, dataFabricacao, dataValidade, quantidadeEstoque) 
+INSERT INTO produto (nome, unidade_medida , valor_unitario , status, url_fotos, peso, data_fabricacao, data_validade, quantidade_estoque)
 VALUES('Pepitas com Açucar Mascavo', 'gr', 47.90, true, 'https://e7.pngegg.com/pngimages/372/321/png-clipart-chocolate-truffle-gaufrette-chocolate-truffle-chocolate.png', 100, '2021-11-08', '2024-11-08', 22);
 
 select * from produto;
@@ -77,24 +80,25 @@ select * from cliente;
 
 -- insercao de pedido
 
-insert into pedido (datapedido, Codigocliente, enderecoEntrega, aberto, valorTotal, quantidadeTotal, percentualDesconto) 
+insert into pedido (data_pedido, codigo_cliente, endereco_entrega, aberto, valor_total, quantidade_total, percentual_desconto) 
 values ('2021-11-08', 1, 'Rua Capitão João Urias, 984, Centro, Águas da Prata/SP, 13890-972', true, 35.00, 10, 5);
-insert into pedido (datapedido, Codigocliente, enderecoEntrega, aberto, valorTotal, quantidadeTotal, percentualDesconto) 
+insert into pedido (data_pedido, codigo_cliente, endereco_entrega, aberto, valor_total, quantidade_total, percentual_desconto) 
 values ('2021-11-08', 2, 'Avenida João F. Abreu, 180, Setor Central, Abreulândia/TO, 77693-970', true, 45.80, 2, 5);
-insert into pedido (datapedido, Codigocliente, enderecoEntrega, aberto, valorTotal, quantidadeTotal, percentualDesconto) 
+insert into pedido (data_pedido, codigo_cliente, endereco_entrega, aberto, valor_total, quantidade_total, percentual_desconto)
 values ('2021-11-08', 3, 'Travessa Nossa Senhora da Aparecida, 599, Getúlio Vargas, Barra Mansa/RJ, 27325-530', true, 479.00, 10, 5);
 
 -- insercao de item pedido
 
-insert into itemPedido (quantidade, valorTotal, codigoPedido, codigoProduto) values (10, 33.25, 1, 1);
-insert into itemPedido (quantidade, valorTotal, codigoPedido, codigoProduto) values (2, 43.51, 2, 2);
-insert into itemPedido (quantidade, valorTotal, codigoPedido, codigoProduto) values (10, 455.05, 3, 3);
+insert into itemPedido (quantidade, valorTotal, codigo_pedido, codigo_produto) values (10, 33.25, 1, 1);
+insert into itemPedido (quantidade, valorTotal, codigo_pedido, codigo_produto) values (2, 43.51, 2, 2);
+insert into itemPedido (quantidade, valorTotal, codigo_pedido, codigo_produto) values (10, 455.05, 3, 3);
+
 
 -- atualização do item pedido
 
-update produto set quantidadeEstoque=quantidadeEstoque -10 where codigo = 1;
-update produto set quantidadeEstoque=quantidadeEstoque -2 where codigo = 2;
-update produto set quantidadeEstoque=quantidadeEstoque -10 where codigo = 3;
+update produto set quantidade_estoque=quantidade_estoque -10 where codigo =1;
+update produto set quantidade_estoque=quantidade_estoque -2 where codigo =2;
+update produto set quantidade_estoque=quantidade_estoque -10 where codigo =3;
 
 
 select * from itemPedido;
