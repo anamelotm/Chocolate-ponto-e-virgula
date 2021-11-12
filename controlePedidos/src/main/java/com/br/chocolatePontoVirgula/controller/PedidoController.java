76ad5346ef0b7fa9f985be3e9e05ce68c9da7d26
IsabelOliv <vargas.oliveira.94@gmail.com>
2021-11-12 14:20:55 -0300

    Pedido controller

--- controlePedidos/src/main/java/com/br/chocolatePontoVirgula/controller/PedidoController.java
+++ controlePedidos/src/main/java/com/br/chocolatePontoVirgula/controller/PedidoController.java
@@ -1,9 +1,8 @@
 package com.br.chocolatePontoVirgula.controller;
 
-import com.br.chocolatePontoVirgula.model.repository.PedidoRepository;
+import com.br.chocolatePontoVirgula.model.services.PedidoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,48 +17,30 @@ import java.util.List;
 public class PedidoController {
 
     @Autowired
-    private PedidoRepository pedidoRepository;
+    private PedidoService pedidoService;
 
     @PostMapping
-    public @ResponseBody
-    String addNewUser(@RequestParam Date dataPedido
-            , @RequestParam int codigoCliente, @RequestParam int percentualDesconto,
-                      @RequestParam boolean aberto,@RequestParam String enderecoEntrega,
-                      @RequestParam double valorTotal,@RequestParam int quantidadeTotal) {
-        // @ResponseBody means the returned String is the response, not a view name
-        // @RequestParam means it is a parameter from the GET or POST request
-
-        Pedido pedido = new Pedido();
-        pedido.setDataPedido(dataPedido);
-        pedido.setCodigoCliente(codigoCliente);
-        pedido.setPercentualDesconto(percentualDesconto);
-        pedido.setAberto(aberto);
-        pedido.setEnderecoEntrega(enderecoEntrega);
-        pedido.setValorTotal(valorTotal);
-        pedido.setQuantidadeTotal(quantidadeTotal);
-        pedidoRepository.save(pedido);
-        return "Saved";
+    public void save(@RequestBody Pedido pedido){
+        pedidoService.save(pedido);
     }
 
-    @GetMapping
-    public ResponseEntity<Page<Pedido>> ListAll(Pageable pageable) {
-
-        int size = 10;
-        PageRequest pageRequest = PageRequest.ofSize(size);
-
-        Page<Pedido> result = pedidoRepository.findAll(pageRequest);
-        return ResponseEntity.ok(result);
+    @PutMapping("/{id}")
+    public void update(@PathVariable Long id, @RequestBody Pedido pedido){
+        pedidoService.update(id, pedido);
     }
-    @DeleteMapping("/{codigo}")
-    public void excluir(@PathVariable long codigo){
-        pedidoRepository.deleteById(codigo);
+
+    @DeleteMapping("/{id}")
+    public void excluir(@PathVariable Long id){
+        pedidoService.excluir(id);
     }
-    @PutMapping("/{codigo}")
-    public void alterar(@PathVariable long codigo, @RequestBody Pedido pedido){
-        Pedido pedidoPesquisado = pedidoRepository.getOne(codigo);
-        if(pedidoPesquisado != null){
-            pedidoPesquisado.setAberto(pedido.isAberto());
-            pedidoRepository.save(pedidoPesquisado);
-        }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Pedido> findById(@PathVariable Long id) {
+        return pedidoService.findById(id);
+    }
+
+    @GetMapping()
+    public ResponseEntity<Page<Pedido>> findAll( Pageable pageable) {
+        return pedidoService.findAll(pageable);
     }
 }
