ed417a384fe73a998f3768004066526680bca771
Ana Caroline <anacarolinevilela0@gmail.com>
2021-11-12 11:31:59 -0300

    inserindo a annotation @Service

--- controlePedidos/src/main/java/com/br/chocolatePontoVirgula/model/services/ItemPedidoService.java
+++ controlePedidos/src/main/java/com/br/chocolatePontoVirgula/model/services/ItemPedidoService.java
@@ -9,7 +9,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 
+@Service
 public class ItemPedidoService {
 
     @Autowired
