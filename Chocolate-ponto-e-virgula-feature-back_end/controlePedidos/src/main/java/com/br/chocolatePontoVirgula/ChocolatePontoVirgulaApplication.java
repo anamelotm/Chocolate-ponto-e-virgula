977740ab89dca08fd94495824fb276f5b11c1519
evelin-kashimir <evelin.kashimir@gmail.com>
2021-11-18 09:03:58 -0300

    adicionado o arquivo back-end para testes

---
+++ Chocolate-ponto-e-virgula-feature-back_end/controlePedidos/src/main/java/com/br/chocolatePontoVirgula/ChocolatePontoVirgulaApplication.java
@@ -0,0 +1,13 @@
+package com.br.chocolatePontoVirgula;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChocolatePontoVirgulaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChocolatePontoVirgulaApplication.class, args);
+	}
+
+}
---
+++ Chocolate-ponto-e-virgula-feature-back_end/controlePedidos/src/main/java/com/br/chocolatePontoVirgula/model/repository/ItemPedidoRepository.java
@@ -0,0 +1,9 @@
+package com.br.chocolatePontoVirgula.model.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.br.chocolatePontoVirgula.model.entity.ItemPedido;
+
+public interface ItemPedidoRepository extends JpaRepository<ItemPedido, Long>{
+
+}
