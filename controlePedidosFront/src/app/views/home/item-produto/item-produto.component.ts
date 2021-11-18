04a4495495850d6e928463cf4742f17ca0580f31
AnaMelo <analuisa.tomita@hotmail.com>
2021-11-18 13:19:39 -0300

    melhoria carrinho

--- controlePedidosFront/src/app/views/home/item-produto/item-produto.component.ts
+++ controlePedidosFront/src/app/views/home/item-produto/item-produto.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { MessengerService } from 'src/app/services/messenger.service';
 import { ProdutoService } from 'src/app/services/produto.service';
 import { Produto } from 'src/app/shared/models/produto';
+import { CarrinhoService } from '../../../services/carrinho.service';
 
 @Component({
   selector: 'app-item-produto',
@@ -13,12 +14,23 @@ export class ItemProdutoComponent implements OnInit {
 
   produtos: Produto[] = [];
 
-  constructor(private msg: MessengerService,
-    private servico: ProdutoService) { }
+  constructor(
+    private msg: MessengerService,
+    private servico: ProdutoService,
+    private cartService: CarrinhoService
+   ) { }
 
   ngOnInit(): void {
-    this.servico.listarProdutos().subscribe(obj => this.produtos = obj);
+    this.servico.listarProdutos().subscribe(obj => {
+      this.produtos = obj;
+
+      this.produtos.forEach((a:any) => {
+        Object.assign(a,{quantity:1, total:a.price});
+      });
+    })
   }
 
-
+  addToCart(item: any){
+    this.cartService.addtoCart(item);
+  }
 }
