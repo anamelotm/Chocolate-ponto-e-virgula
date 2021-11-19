c3f2d91b2e4abdce6ac9c8a134e2d4b8fa6f52bd
evelin-kashimir <evelin.kashimir@gmail.com>
2021-11-19 10:53:13 -0300

    teste de conexao

--- Chocolate-ponto-e-virgula-feature-back_end/controlePedidos/src/main/java/com/br/chocolatePontoVirgula/model/repository/ClienteRepository.java
+++
@@ -1,9 +0,0 @@
-package com.br.chocolatePontoVirgula.model.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.br.chocolatePontoVirgula.model.entity.Cliente;
-
-public interface ClienteRepository extends JpaRepository<Cliente, Long> {
-
-}
