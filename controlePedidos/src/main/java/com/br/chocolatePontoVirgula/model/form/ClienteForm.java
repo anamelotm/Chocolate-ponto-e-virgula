2b8b16cf60dc37e06186d4c3c58348c71909b6a5
Ana Caroline <anacarolinevilela0@gmail.com>
2021-11-21 15:27:40 -0300

    Form - Cliente

---
+++ controlePedidos/src/main/java/com/br/chocolatePontoVirgula/model/form/ClienteForm.java
@@ -0,0 +1,11 @@
+package com.br.chocolatePontoVirgula.model.form;
+
+import lombok.Getter;
+
+@Getter
+public class ClienteForm {
+
+    private String tipo;
+    private String documento;
+    private String nome;
+}
