7241436aa91dca689088d2a2106ca996279b87d3
IsabelOliv <vargas.oliveira.94@gmail.com>
2021-11-22 19:19:45 -0300

    criação do método para retornar o número total de páginas

--- controlePedidos/src/main/java/com/br/chocolatePontoVirgula/controller/PedidoController.java
+++ controlePedidos/src/main/java/com/br/chocolatePontoVirgula/controller/PedidoController.java
@@ -3,10 +3,12 @@ package com.br.chocolatePontoVirgula.controller;
 import com.br.chocolatePontoVirgula.model.dto.PedidoDTO;
 import com.br.chocolatePontoVirgula.model.dto.PedidoFormDTO;
 import com.br.chocolatePontoVirgula.model.dto.PedidosDTO;
+import com.br.chocolatePontoVirgula.model.entity.Cliente;
 import com.br.chocolatePontoVirgula.model.form.PedidoForm;
 import com.br.chocolatePontoVirgula.model.services.PedidoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
@@ -72,7 +74,12 @@ public class PedidoController {
         return PedidosDTO.converter(pedidosLista);
 
     }
-
+    @GetMapping("totaldepaginas")
+    public int retornaTotalpaginas(){
+        Pageable pageable=PageRequest.of(0,10);
+        Page<Pedido> pedidosLista = pedidoService.findAll(pageable);
+        return pedidosLista.getTotalPages();
+    }
     @GetMapping("cliente/{idCliente}")
     public List<Pedido> consultaPedidoCliente(@PathVariable Long idCliente) {
         return pedidoService.consultaPedidosCliente(idCliente);
