fcf8bf748e9b3a4a640e1cfffbb57e06be25a27c
evelin-kashimir <evelin.kashimir@gmail.com>
2021-11-22 10:04:33 -0300

    apagando arquivos repetitivos

--- Chocolate-ponto-e-virgula-feature-back_end/controlePedidos/src/main/java/com/br/chocolatePontoVirgula/model/repository/ItemPedidoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.br.chocolatePontoVirgula.model.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.br.chocolatePontoVirgula.model.entity.ItemPedido;
-
-public interface ItemPedidoRepository extends JpaRepository<ItemPedido, Long>{
-
-}
