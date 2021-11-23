1aa13e8a649504ba7cd07d25f6cb406ea5fe3abd
Ana Caroline <anacarolinevilela0@gmail.com>
2021-11-23 11:58:28 -0300

    retirando a validacao

--- controlePedidos/src/main/java/com/br/chocolatePontoVirgula/model/form/ItemPedidoForm.java
+++ controlePedidos/src/main/java/com/br/chocolatePontoVirgula/model/form/ItemPedidoForm.java
@@ -4,20 +4,21 @@ import com.br.chocolatePontoVirgula.model.entity.Produto;
 import lombok.Getter;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @Getter
 public class ItemPedidoForm {
 
-    @NotBlank(message = "Deve estar associado a um pedido")
+
     private Long idPedido;
 
-    @NotBlank(message = "Deve conter um produto")
+
     private Produto produto;
 
-    @NotBlank(message = "Deve informar a quantidade")
+
     private int quantidade;
 
-    @NotBlank(message = "Deve possuir um valor total")
+  
     private double valorTotal;
 
 }
