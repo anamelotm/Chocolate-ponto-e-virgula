9fad88850cad4137f667e905b44782dd0b5cfa8c
IsabelOliv <vargas.oliveira.94@gmail.com>
2021-11-24 10:38:11 -0300

    adicionada exceçÃão ao não encontrar pedido

---
+++ controlePedidos/src/main/java/com/br/chocolatePontoVirgula/controller/exceptions/ControllerExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.br.chocolatePontoVirgula.controller.exceptions;
+
+import com.br.chocolatePontoVirgula.model.services.exceptions.EntityNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import javax.servlet.http.HttpServletRequest;
+import java.sql.Timestamp;
+import java.time.Instant;
+
+@ControllerAdvice
+public class ControllerExceptionHandler {
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<StandardError> entityNotFound(EntityNotFoundException e, HttpServletRequest request){
+        StandardError err=new StandardError();
+        err.setTimestamp(Instant.now());
+        err.setStatus(HttpStatus.NOT_FOUND.value());
+        err.setError("Recurso não encontrado");
+        err.setMessage(e.getMessage());
+        err.setPath(request.getRequestURI());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+    }
+}
